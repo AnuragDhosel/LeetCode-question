9078edd4444a7ab576cb9bcbd4e70bb3303acb21
Anurag dhosel <anuragdhosel04@gmail.com>
2026-03-16 12:57:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,34 +1,25 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> ans = new ArrayList<>();
-        
+
         int k = p.length();
-        HashMap<Character , Integer> map = new HashMap<>();
-        for(int i=0; i<p.length(); i++){
-            char c = p.charAt(i);
-            if(!map.containsKey(c)) map.put(c , 1);
-            else map.put(c , map.get(c)+1);
+        for(int i=0; i<=s.length()-k; i++){
+            String sub = s.substring(i , k+i);
+            if(areAnagram(sub , p)) ans.add(i);
         }
-
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-
-            if(map.containsKey(c)) map.put(c , map.get(c)-1);
-        
-            if(i >= k-1){
-                int count = 0;
-                for(char key : map.keySet()){
-                    if(map.get(key) == 0) count++;
-                }
-                if(count == map.size()) ans.add(i-k+1);
-
-                if(map.containsKey(s.charAt(i-k+1))){
-                    map.put(s.charAt(i-k+1) , map.get(s.charAt(i-k+1))+1);
-                } 
-            }
-            
-        }
-
         return ans;
+    }
+
+    public boolean areAnagram(String s1 , String s2){
+        char [] arr1 = s1.toCharArray();
+        char [] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        for(int i=0; i<arr1.length; i++){
+            if(arr1[i] != arr2[i]) return false;
+        }
+        return true;
     }
 }
