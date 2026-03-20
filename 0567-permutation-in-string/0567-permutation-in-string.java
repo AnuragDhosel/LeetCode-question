dada22ca538f5bd9e005378645b33ff17e3d6e9a
Anurag dhosel <anuragdhosel04@gmail.com>
2026-03-20 20:26:15 +0530

    Time: 26 ms (30.6%), Space: 46.7 MB (26.49%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,27 +1,36 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        HashMap<Character , Integer> map = new HashMap<>();
+        for(int i=0; i<s1.length(); i++){
+            char c = s1.charAt(i);
+            if(!map.containsKey(c)) map.put(c , 1);
+            else map.put(c , map.get(c)+1);
+        }
+
         int k = s1.length();
         int l = 0;
-        for(int i=k; i<=s2.length(); i++){
-            String sub = s2.substring(l , i);
-            if(isPermuted(s1 , sub)){
-                return true;
+        for(int r=0; r<s2.length(); r++){
+            char c = s2.charAt(r);
+
+            if(map.containsKey(c)){
+                map.put(c , map.get(c)-1);
+            } 
+
+            if(r-l+1 == k){
+                if(isMapZero(map)) return true;
+
+                // shrink
+                char c1 = s2.charAt(l);
+                if(map.containsKey(c1)) map.put(c1 , map.get(c1)+1);
+                l++;
             }
-            l++;
         }
         return false;
     }
-    public boolean isPermuted(String s , String t){
-        char [] arr1 = s.toCharArray();
-        char [] arr2 = t.toCharArray();
-
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        for(int i=0; i<arr1.length; i++){
-            if(arr1[i] != arr2[i]) return false;
+    public boolean isMapZero(HashMap<Character , Integer> map){
+        for(char key : map.keySet()){
+            if(map.get(key) != 0) return false;
         }
-
         return true;
     }
 }
