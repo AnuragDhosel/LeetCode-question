8818307ab20d4094c5678ec3219fc6a07aa2328a
Anurag dhosel <anuragdhosel04@gmail.com>
2026-03-31 15:49:42 +0530

    Time: 1 ms (24.43%), Space: 42.6 MB (80.8%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,21 +1,42 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        int n = s.length();
-        if(n != goal.length()) return false;
+        if(s.length() != goal.length()) return false;
+        
+        String s2 = s + s;
 
-        for(int st=0; st<s.length(); st++){
+        int [] lps = calculateLPS(s2);
 
-            int i = st; // for s string
-            int j = 0; // for goal string
-            while(j < goal.length()){
-                if(s.charAt(i % n) == goal.charAt(j)){
-                    i++;
-                    j++;
-                }
-                else break;
+        // search goal in s2 -> use KMP
+        int i = 0 , j = 0;
+        while(i < s2.length()){
+            if(s2.charAt(i) == goal.charAt(j)){
+                i++;
+                j++;
+                if(j == goal.length()) return true;
             }
-            if(j == goal.length()) return true;
+            else{ // s2.charAt(i) != goal.charAt(j)
+                if(j != 0) j = lps[j-1];
+                else i++;
+            }
         }
         return false;
+    }
+    public int[] calculateLPS(String s){
+        int [] lps = new int[s.length()];
+
+        int i = 1;
+        int len = 0;
+        while(i < s.length()){
+            if(s.charAt(i) == s.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else{
+                if(len != 0) len = lps[len - 1];
+                else i++;
+            }
+        }
+        return lps;
     }
 }
