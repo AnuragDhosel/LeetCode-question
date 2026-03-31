d929ef1a9fc32d6acae23222a26a3a7b10d69bf5
Anurag dhosel <anuragdhosel04@gmail.com>
2026-03-31 11:14:53 +0530

    Time: 12 ms (24.23%), Space: 47.5 MB (84.22%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestPrefix(String s) {
+        int [] lps = new int[s.length()];
+
+        int i = 1;
+        int len = 0;
+        while(i < s.length()){
+            if(s.charAt(i) == s.charAt(len)){
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else{
+                if(len != 0) len = lps[len - 1];
+                else i++;
+            }
+        }
+
+        int l = lps[lps.length - 1];
+        return s.substring(0 , l);
+    }
+}
