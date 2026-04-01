2031467daaa2401d0816b63e5c35222ac2d7023b
Anurag dhosel <anuragdhosel04@gmail.com>
2026-04-01 18:59:50 +0530

    Time: 259 ms (82.14%), Space: 43.5 MB (75.64%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -3,13 +3,16 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int count = 0;
 
+        // Step 1: Keep appending until length >= b
         while(sb.length() < b.length()){
             sb.append(a);
             count++;
         }
 
+        // Step 2: Check
         if(sb.indexOf(b) != -1) return count;
 
+        // Step 3: One more append (important case)
         sb.append(a);
         count++;
         if(sb.indexOf(b) != -1) return count;
