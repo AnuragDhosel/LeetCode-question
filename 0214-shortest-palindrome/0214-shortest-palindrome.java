a80ef7a5a1a6463f48cdbe8e1cf9bbdbb9f38e79
Anurag dhosel <anuragdhosel04@gmail.com>
2026-04-02 11:26:28 +0530

    Time: 8 ms (51.1%), Space: 47.2 MB (20.6%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,16 +1,26 @@
 class Solution {
     public String shortestPalindrome(String s) {
+        // Step 1:
+        // Create a new string -> original + '#' + reversed
+        // '#' is used as a separator to avoid overlap confusion
         String newStr = s + '#' + reverse(s);
-        System.out.println(newStr);
 
+        // Step 2:
+        // Build LPS array (KMP preprocessing)
+        // LPS[i] = longest prefix which is also suffix till index i
         int [] lps = calculateLPS(newStr);
 
+        // Step 3:- IMP
+        // Last value of LPS gives:
+        // length of longest prefix of original string which is palindrome
+        // Extract remaining part (which is NOT part of palindrome prefix)
         String sub = s.substring(lps[lps.length-1] , s.length());
-        System.out.println(sub);
+
+        // Step 4;
+        // reverse the string
         String rev = reverse(sub);
 
         return rev + s;
-
     }
     public String reverse(String s){
         StringBuilder sb = new StringBuilder();
