4453cc7d24b5eb0a40f62af93b024d441a3ff8a8
Anurag dhosel <anuragdhosel04@gmail.com>
2026-04-02 11:03:21 +0530

    Time: 1 ms (90.94%), Space: 43 MB (79.44%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.java
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.java
@@ -1,28 +1,44 @@
 class Solution {
     public int maxRepeating(String sequence, String word) {
-        int n = sequence.length();
-        int m = word.length();
-        int ans = 0;
-
-        for(int x=0; x<=n-m; x++){
-            int count = 0;
-            int maxC = 0;
-            int i = x;
-            while(i <= n-m){
-                String sub = sequence.substring(i , i+m);
-                if(sub.equals(word)){
-                    count++;
-                    i += m;
-                }
-                else{
-                    maxC = Math.max(count , maxC);
-                    count = 0;
-                    i++;
-                }   
-            }
-            maxC = Math.max(count , maxC);
-            ans = Math.max(maxC , ans);
+        int count = 0;
+        String temp = word;
+        
+        while (kmpSearch(sequence, temp)) {
+            count++;
+            temp += word;
         }
-        return ans;
+        
+        return count;
+    }
+
+    private boolean kmpSearch(String text, String pattern) {
+        int[] lps = buildLPS(pattern);
+        int i = 0, j = 0;
+
+        while (i < text.length()) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++; j++;
+                if (j == pattern.length()) return true;
+            } else {
+                if (j != 0) j = lps[j - 1];
+                else i++;
+            }
+        }
+        return false;
+    }
+
+    private int[] buildLPS(String s) {
+        int[] lps = new int[s.length()];
+        int i = 1, len = 0;
+
+        while (i < s.length()) {
+            if (s.charAt(i) == s.charAt(len)) {
+                lps[i++] = ++len;
+            } else {
+                if (len != 0) len = lps[len - 1];
+                else i++;
+            }
+        }
+        return lps;
     }
 }
