d579399019a8fb30eae75d616c0d4245f1e397b6
Anurag dhosel <anuragdhosel04@gmail.com>
2026-04-03 12:21:13 +0530

    Time: 5 ms (97.6%), Space: 42.6 MB (96.88%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,24 +1,20 @@
 class Solution {
+    int count = 0;
     public int countSubstrings(String s) {
-        int count = 0;
         for(int i=0; i<s.length(); i++){
-            // for odd length
-            count += countPalindrome(s , i , i);
-
-            // for even length
-            count += countPalindrome(s , i , i+1);
+            // for odd
+            checkPalindrome(s , i , i);
+            // for even
+            checkPalindrome(s , i , i+1);
         }
 
         return count;
     }
-    public int countPalindrome(String s  , int l , int r){
-        int count = 0;
-        while(l >= 0 && r < s.length()){
-            if(s.charAt(l) != s.charAt(r)) return count;
-            count++;
+    public void checkPalindrome(String s , int l , int r){
+        while(l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
             l--;
             r++;
+            count++;
         }
-        return count;
     }
 }
