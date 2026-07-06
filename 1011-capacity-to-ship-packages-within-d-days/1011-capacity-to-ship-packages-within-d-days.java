f42e62942f9201be1438cea5b5acf03e28a69e07
Anurag dhosel <anuragdhosel04@gmail.com>
2026-07-06 23:07:19 +0530

    Time: 12 ms (28.16%), Space: 50.3 MB (19.71%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int st = maximumValue(weights);
+        int end = sumOfArray(weights);
+        int ans = 0;
+
+        while(st <= end){
+            int mid = st + (end - st)/2; // capacity
+
+// calculate How Many Days It Takes on Converyor Belt for shipped
+            int takeDay = calculateDaysRequired(weights , mid);
+
+            if(takeDay <= days){
+                ans = mid;
+                end = mid - 1;
+            }
+            else{
+                st = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int calculateDaysRequired(int [] weights , int shipCapi){
+        int takeDays = 1;
+        int sum = 0;
+        for(int i=0; i<weights.length; i++){
+            if(sum + weights[i] <= shipCapi){
+                sum += weights[i];
+            }
+            else{
+                takeDays++;
+                sum = weights[i];
+            }
+        }            
+        
+        return takeDays;
+    }
+
+    public int sumOfArray(int [] arr){
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+    public int maximumValue(int [] arr){
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            max = Math.max(arr[i] , max);
+        }
+        return max;
+    }
+}
