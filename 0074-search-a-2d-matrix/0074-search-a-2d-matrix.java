568179b1d498ada4116454766e490fcdbb721728
Anurag dhosel <anuragdhosel04@gmail.com>
2026-07-14 09:33:04 +0530

    Time: 0 ms (100%), Space: 44.2 MB (8.67%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,52 +1,24 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if(target > matrix[matrix.length-1][matrix[0].length-1]){
-            return false;
-        }
-        
-        int ansR = findTargetRow(matrix , target);
-        if(ansR == -1) return false;
+        int row = matrix.length;
+        int col = matrix[0].length;
 
-        return BSinMatrix(matrix , ansR , target);
+        int r = 0;
+        int c = col - 1;
 
-    }
-    public int findTargetRow(int [][] matrix , int target){
-        int st = 0;
-        int end = matrix.length-1;
-        int ansR = -1;
+        while(r < row && c >= 0){
+            int mid = matrix[r][c];
 
-        while(st <= end){
-            int mid = st + (end - st)/2;
-
-            if(matrix[mid][0] <= target){
-                ansR = mid;
-                st = mid + 1;
-            }
-            else{
-                end = mid - 1;
-            }
-        }
-        return ansR;
-    }
-
-    public boolean BSinMatrix(int [][] matrix , int ansR , int target){
-        int st = 0;
-        int end = matrix[0].length-1;
-        
-        while(st <= end){
-            int mid = st + (end - st)/2;
-
-            if(matrix[ansR][mid] == target){
+            if(target == mid){
                 return true;
             }
-            else if(matrix[ansR][mid] > target){
-                end = mid - 1;
+            else if(target > mid){
+                r++;
             }
-            else{ // matrix[ansC][mid] < target
-                st = mid + 1;
+            else{ // target < mid
+                c--;
             }
         }
-
         return false;
     }
 }
