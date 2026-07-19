2d634eb3869021de08ef4f68128528cd9aaf307c
Anurag dhosel <anuragdhosel04@gmail.com>
2026-07-20 01:52:40 +0530

    Time: 1 ms (92.82%), Space: 42.6 MB (50.79%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -4,10 +4,11 @@ class Solution {
             return Integer.MAX_VALUE;
         }
 
-        boolean negative = false;
-        if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)){
-            negative = true;
-        }
+        // boolean negative = false;
+        // if((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)){
+        //     negative = true;
+        // }
+        boolean negative = (dividend < 0) ^ (divisor < 0);
 
         long m = Math.abs((long) dividend);
         long n = Math.abs((long) divisor);
@@ -15,20 +16,16 @@ class Solution {
         long ans = 0;
         while(m >= n){
             int count = 0;
-            while(m >= n * (long)Math.pow(2 , count+1)){
+            while(m >= (n << (count + 1))){
                 count++;
-            }
-            ans += (long)Math.pow(2 , count);
-            m -= n * (long)Math.pow(2 , count);
+            }  
+            ans += (1 << count); 
+            m -= (n << count);
         }   
-
-        // System.out.println(ans);
-        // // System.out.println(Integer.MAX_VALUE + 1);
-        // if(ans-1 == Integer.MAX_VALUE){
-        //     System.out.println("T");
-        //     return negative ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        // }
 
         return negative ? (int)-ans : (int)ans;
     }
 }
+
+// (n << (count + 1)) => n * (long)Math.pow(2 , count+1)
+// 1 << count => (long)Math.pow(2 , count)
