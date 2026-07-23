b6404c2a1530e20a2b26bbe203e803cc0c3f3284
Anurag dhosel <anuragdhosel04@gmail.com>
2026-07-23 22:17:27 +0530

    Time: 0 ms (100%), Space: 43.1 MB (8.23%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,39 +3,42 @@ class Solution {
         int n = nums.length;
         if(n == 1) return nums[0];
 
-        int [] dp = new int[n+1];
-
-        int taking_0th_idx = solve1(nums , n-1 , dp);
-        Arrays.fill(dp , 0);
-        int taking_last_idx = solve2(nums , n , dp);
+        int taking_0th_idx = solve1(nums , n-1);
+        int taking_last_idx = solve2(nums , n);
 
         return Math.max(taking_0th_idx , taking_last_idx);
     }
-    public int solve1(int [] nums , int size , int [] dp){
-        dp[0] = 0;
-        dp[1] = nums[0]; // take 1st house
+    public int solve1(int [] nums , int size){
+        int prevPrev = 0;
+        int prev = nums[0]; // take 1st house
 
         for(int i=2; i<=size; i++){
-            int pick = nums[i-1] + dp[i-2];
-            int skip = dp[i-1];
+            int pick = nums[i-1] + prevPrev;
+            int skip = prev;
 
-            dp[i] = Math.max(pick , skip);
+            int curr = Math.max(pick , skip);
+
+            prevPrev = prev;
+            prev = curr;
         }
 
-        return dp[size];
+        return prev;
     }
 
-    public int solve2(int [] nums , int size , int [] dp){
-        dp[0] = 0;
-        dp[1] = 0; // skip 1st house
+    public int solve2(int [] nums , int size){
+        int prevPrev = 0;
+        int prev = 0; // skip 1st house
 
         for(int i=2; i<=size; i++){
-            int pick = nums[i-1] + dp[i-2];
-            int skip = dp[i-1];
+            int pick = nums[i-1] + prevPrev;
+            int skip = prev;
 
-            dp[i] = Math.max(pick , skip);
+            int curr = Math.max(pick , skip);
+
+            prevPrev = prev;
+            prev = curr;
         }
 
-        return dp[size];
+        return prev;
     } 
 }
