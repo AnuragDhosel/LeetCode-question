8b736fbed20d253e8c68f13291c8b460883d8d7a
Anurag dhosel <anuragdhosel04@gmail.com>
2026-07-29 15:35:31 +0530

    Time: 0 ms (100%), Space: 42.6 MB (90.51%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -3,29 +3,25 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
-
-        dp = new int[n+1];
-        Arrays.fill(dp , -1);
         
-        // take 0th idx element but not take last element
-        int taking_0th_idxEle = solve(nums , 0 , n-2);
+        dp = new int[n+1];
 
         Arrays.fill(dp , -1);
-        // take last idx element but not take 0th idx element
-        int taking_last_idxEle = solve(nums , 1 , n-1);
+        int taking_0th_idx = solve(nums , 0 , n-1);
 
-        return Math.max(taking_0th_idxEle , taking_last_idxEle);
-    }
+        Arrays.fill(dp , -1);
+        int taking_last_idx = solve(nums , 1 , n);
+
+        return Math.max(taking_0th_idx , taking_last_idx);
+    }   
     public int solve(int [] nums , int i , int size){
-        if(i > size) return 0;
+        if(i >= size) return 0;
 
         if(dp[i] != -1) return dp[i];
 
-        int pick = nums[i] + solve(nums , i+2 , size);
+        int pick = solve(nums , i+2 , size) + nums[i];
         int skip = solve(nums , i+1 , size);
 
-        dp[i] = Math.max(pick , skip);
-
-        return dp[i];
+        return dp[i] = Math.max(pick , skip);
     }
 }
