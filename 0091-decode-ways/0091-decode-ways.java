d741c3e65475dde572283c0db762684562269723
Anurag dhosel <anuragdhosel04@gmail.com>
2026-07-29 12:17:00 +0530

    Time: 0 ms (100%), Space: 43.3 MB (33.35%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,31 +1,27 @@
 class Solution {
-    int [] dp;
-    public int solve(int i , int n , String s){
-        if(i >= n) // we get a valid way
-            return 1;
+    public int numDecodings(String s) {
+        int n = s.length();
 
-        if(s.charAt(i) == '0') // we get a unvalid way bcz 0 can't represent
-            return 0;
+        int [] dp = new int[n+1];
+        dp[n] = 1;  // bcz in top-down : if(i == n) return 1
 
-        if(dp[i] != -1) return dp[i];    
+        for(int i=n-1; i>=0; i--){ // opposite bcz dp[i] comes from dp[i+1]
+            if(s.charAt(i) == '0'){ // bcz in top-down : if(s[i] == 0) return 0
+                dp[i] = 0;
+            }
+            else{
+                int oneEleChoosen = dp[i+1]; // bcz of solve(i+1)
 
-        int oneEleChoosen = solve(i + 1 , n ,  s);
+                int twoEleChoosen = 0;
+                if(i + 1 < n){
+                    if(s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) <= '6'))
+                        twoEleChoosen = dp[i+2]; // bcz of solve(i+2)
+                }
 
-        int twoEleChoosen = 0;
-        if(i + 1 < n){
-            if(s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) <= '6')){
-                twoEleChoosen = solve(i + 2 , n , s);
+                dp[i] = oneEleChoosen + twoEleChoosen;
             }
         }
 
-        dp[i] = oneEleChoosen + twoEleChoosen;
-        return dp[i];
-    }
-
-    public int numDecodings(String s) {
-        dp = new int[s.length() + 1];
-        Arrays.fill(dp , -1);
-
-        return solve(0 , s.length() , s);
+        return dp[0];
     }
 }
