125efac60dcbf467564ec7b5a343e6d9c5106da9
Anurag dhosel <anuragdhosel04@gmail.com>
2026-07-31 23:18:43 +0530

    Time: 0 ms (100%), Space: 43 MB (98.91%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -12,12 +12,14 @@ class Solution {
         dp[0][0] = 1; // total ways to reach from 0,0 to 0,0 is 1
 
         for(int col=1; col<n; col++){ // fill the 1st row
-            if(obstacleGrid[0][col] == 1) break;
+            if(obstacleGrid[0][col] == 1) 
+                break;
             dp[0][col] = 1;
         }
 
         for(int row=1; row<m; row++){ // fill the 1st col
-            if(obstacleGrid[row][0] == 1) break;
+            if(obstacleGrid[row][0] == 1) 
+                break;
             dp[row][0] = 1;
         }
 
