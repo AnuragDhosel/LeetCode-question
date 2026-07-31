fe4818cc873bdb2683b8111ea3424b7990dd750c
Anurag dhosel <anuragdhosel04@gmail.com>
2026-07-31 18:39:51 +0530

    Time: 0 ms (100%), Space: 42.1 MB (48.53%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,25 @@
+class Solution {
+    int [][] dp;
+    public int solve(int row , int col){
+        if(row == 0 && col == 0) // we get a valid ans
+            return 1;
+        if(row < 0 || col < 0)
+            return 0;
+
+        if(dp[row][col] != -1)
+            return dp[row][col];
+
+        int up = solve(row-1 , col);
+        int left = solve(row , col-1);
+
+        dp[row][col] = up + left;
+        return dp[row][col];
+    }
+    public int uniquePaths(int m, int n) {
+        dp = new int[m][n];
+        for(int i=0; i<m; i++)
+            Arrays.fill(dp[i] , -1);
+        
+        return solve(m-1 , n-1);
+    }
+}
