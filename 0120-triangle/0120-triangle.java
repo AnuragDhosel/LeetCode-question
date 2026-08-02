98f549e90685d5ae59e101077ca55820dd04d6e4
Anurag dhosel <anuragdhosel04@gmail.com>
2026-08-02 23:58:42 +0530

    Time: 4 ms (41.87%), Space: 45.2 MB (85.71%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,27 +1,20 @@
 class Solution {
-    int [][] dp;
-    public int solve(int row , int col , List<List<Integer>> list){
-        if(row == list.size() - 1)
-            return list.get(row).get(col);
-
-        if(dp[row][col] != Integer.MAX_VALUE)
-            return dp[row][col];    
-
-        int same = list.get(row).get(col) + solve(row+1 , col , list);
-        int ahead = list.get(row).get(col) + solve(row+1 , col+1 , list);
-
-        dp[row][col] = Math.min(same , ahead);    
-
-        return dp[row][col];
-    }
     public int minimumTotal(List<List<Integer>> triangle) {
         int m = triangle.size();
         int n = triangle.get(m-1).size();
 
-        dp = new int[m][n];
-        for(int i=0; i<m; i++)
-            Arrays.fill(dp[i] , Integer.MAX_VALUE);
-    
-        return solve(0 , 0 , triangle);
+        int [][] dp = new int [m][n];
+        
+        for(int col=0; col<m; col++) // initialise last row of dp
+            dp[m-1][col] = triangle.get(m-1).get(col);
+        
+        for(int row=m-2; row>=0; row--){
+            for(int col=0; col<triangle.get(row).size(); col++){
+                int minValueOfPreviRow = Math.min(dp[row+1][col] , dp[row+1][col+1]);
+                dp[row][col] = triangle.get(row).get(col) + minValueOfPreviRow;
+            }
+        }
+
+        return dp[0][0];
     }
 }
