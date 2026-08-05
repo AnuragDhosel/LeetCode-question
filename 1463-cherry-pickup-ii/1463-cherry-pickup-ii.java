16cc304f888160c0d7b29bedb696e26430240df7
Anurag dhosel <anuragdhosel04@gmail.com>
2026-08-05 10:21:30 +0530

    Time: 26 ms (56.48%), Space: 46.2 MB (78.7%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,34 +1,27 @@
 class Solution {
     int [][][] dp;
     public int solve(int row , int col1 , int col2 , int [][] nums){
-        // 1. base case 
-        // if we reached out of the grid
         if(col1 < 0 || col2 < 0 || col1 >= nums[0].length || col2 >= nums[0].length)
             return 0;
-
-        // if we reach last row
         if(row == nums.length-1)
-            return (col1 == col2) ? nums[row][col1] : (nums[row][col1] + nums[row][col2]);
+            return (col1 == col2) ? nums[row][col1] : nums[row][col1] + nums[row][col2];
         
-
-        // 2. apply dp
         if(dp[row][col1][col2] != -1)
             return dp[row][col1][col2];
-        
-        // 3. explore all path for robot1 and robot2 simultanously
-        int max = Integer.MIN_VALUE;
-        int sum = 0;
-        for(int c1 = -1; c1 <= +1; c1++){
-            for(int c2 = -1; c2 <= +1; c2++){
-                sum = (col1 == col2) ? nums[row][col1] : (nums[row][col1] + nums[row][col2]);
 
-                sum += solve(row+1 , col1+c1 , col2+c2 , nums); 
+        int max = 0;
+        int sum = 0;
+        for(int c1 = -1; c1 <= +1 ; c1++){
+            for(int c2 = -1; c2 <= +1; c2++){
+                sum = (col1 == col2) ? nums[row][col1] : nums[row][col1] + nums[row][col2];
+
+                sum += solve(row+1 , col1+c1 , col2+c2 , nums);
+
                 max = Math.max(sum , max);
             }
         }
-        
-        dp[row][col1][col2] = max;
-        return dp[row][col1][col2];
+
+        return dp[row][col1][col2] = max;
     }
     public int cherryPickup(int[][] grid) {
         int m = grid.length;
@@ -43,6 +36,6 @@ class Solution {
             }
         }
 
-        return solve(0, 0, grid[0].length-1 , grid);
+        return solve(0 , 0 , grid[0].length-1 , grid);
     }
 }
