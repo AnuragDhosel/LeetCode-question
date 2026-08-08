0206943e74cd4980ed6d9b49db6e59d3c9574750
Anurag dhosel <anuragdhosel04@gmail.com>
2026-08-09 00:56:45 +0530

    Time: 4 ms (87.87%), Space: 45.1 MB (39.28%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,20 @@
 class Solution {
+    int [][] dp;
     // how many subarrays whose sum is target
     public int solve(int idx , int target , int [] arr){
         if(idx < 0)
             return (0 == target) ? 1 : 0;
+
+        if(dp[idx][target] != -1)
+            return dp[idx][target];
 
         int take = 0;
         if(arr[idx] <= target)
             take = solve(idx-1 , target - arr[idx] , arr);
         int notTake = solve(idx-1 , target , arr);
 
-        return take + notTake;
+        dp[idx][target] = take + notTake;
+        return dp[idx][target];
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
@@ -17,10 +22,19 @@ class Solution {
         for(int i=0; i<n; i++)
             sum += nums[i];
 
+        if(Math.abs(target) > sum)
+            return 0;
+
+        if((target+sum) % 2 != 0) 
+            return 0;
+
         // s1 = (target + sum)/2;
         int realTarget = (target + sum)/2;
 
-        if((target+sum) % 2 != 0) return 0;
+        dp = new int[n][realTarget + 1];
+        for(int i=0; i<n; i++)
+            Arrays.fill(dp[i] , -1);
+        
 
         return solve(n-1 , realTarget , nums);
     }
