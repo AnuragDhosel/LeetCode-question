1fe3a7f8663eee0b7e212e4b63a67038f5322959
Anurag dhosel <anuragdhosel04@gmail.com>
2026-08-09 00:52:49 +0530

    Time: 356 ms (31.02%), Space: 41.9 MB (99.94%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,53 +1,27 @@
 class Solution {
-    int[][] dp;
-    int offset;
+    // how many subarrays whose sum is target
+    public int solve(int idx , int target , int [] arr){
+        if(idx < 0)
+            return (0 == target) ? 1 : 0;
 
-    public int solve(int idx, int target, int[] nums) {
+        int take = 0;
+        if(arr[idx] <= target)
+            take = solve(idx-1 , target - arr[idx] , arr);
+        int notTake = solve(idx-1 , target , arr);
 
-        // Target has gone outside the range we can represent
-        if (target < -offset || target > offset)
-            return 0;
-
-        // Base case
-        if (idx == 0) {
-            if (nums[0] == 0 && target == 0)
-                return 2;
-
-            return nums[0] == Math.abs(target) ? 1 : 0;
-        }
-
-        if (dp[idx][target + offset] != -1)
-            return dp[idx][target + offset];
-
-        int plus = solve(idx - 1, target - nums[idx], nums);
-
-        int minus = solve(idx - 1, target + nums[idx], nums);
-
-        dp[idx][target + offset] = plus + minus;
-
-        return dp[idx][target + offset];
+        return take + notTake;
     }
-
     public int findTargetSumWays(int[] nums, int target) {
-
         int n = nums.length;
-
         int sum = 0;
+        for(int i=0; i<n; i++)
+            sum += nums[i];
 
-        for (int num : nums)
-            sum += num;
+        // s1 = (target + sum)/2;
+        int realTarget = (target + sum)/2;
 
-        // Impossible target
-        if (Math.abs(target) > sum)
-            return 0;
+        if((target+sum) % 2 != 0) return 0;
 
-        offset = sum;
-
-        dp = new int[n][2 * sum + 1];
-
-        for (int i = 0; i < n; i++)
-            Arrays.fill(dp[i], -1);
-
-        return solve(n - 1, target, nums);
+        return solve(n-1 , realTarget , nums);
     }
 }
