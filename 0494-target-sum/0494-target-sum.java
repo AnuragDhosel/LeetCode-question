309334d9b3a18f555ba04aa8eeb5af50bda872ee
Anurag dhosel <anuragdhosel04@gmail.com>
2026-08-09 01:11:44 +0530

    Time: 4 ms (87.97%), Space: 45.1 MB (39.24%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,6 +1,5 @@
 class Solution {
     int [][] dp;
-    // how many subarrays whose sum is target
     public int solve(int idx , int target , int [] arr){
         if(idx < 0)
             return (target == 0) ? 1 : 0;
@@ -16,26 +15,29 @@ class Solution {
         dp[idx][target] = take + notTake;
         return dp[idx][target];
     }
+
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        int sum = 0;
+        int total = 0;
         for(int i=0; i<n; i++)
-            sum += nums[i];
+            total += nums[i];
 
-        if(Math.abs(target) > sum)
+        // impossible to create
+        if(Math.abs(target) > total)
             return 0;
 
-        if((target+sum) % 2 != 0) 
-            return 0;
+        // suppose : target = diff , total = totalSum , S1 = realTarget
+        // so , s1 = (target + sum)/2; represented as
+        int realTarget = (target + total)/2;
 
-        // s1 = (target + sum)/2;
-        int realTarget = (target + sum)/2;
+        if((target + total) % 2 != 0) 
+            return 0;
 
         dp = new int[n][realTarget + 1];
         for(int i=0; i<n; i++)
             Arrays.fill(dp[i] , -1);
         
-
+        // so how many subarrays whose sum is target or realTarget
         return solve(n-1 , realTarget , nums);
     }
 }
