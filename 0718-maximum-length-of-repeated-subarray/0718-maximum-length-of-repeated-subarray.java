c7aace91e3a9fae8b3f3ac9c58c34bc91b082666
Anurag dhosel <anuragdhosel04@gmail.com>
2026-08-20 00:20:54 +0530

    Time: 182 ms (13.83%), Space: 66.8 MB (10.78%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
@@ -1,25 +1,33 @@
 class Solution {
+    int max = -1;
+    int [][] dp;
+    public int solve(int i , int j , int [] arr1 , int [] arr2){
+        if(i < 0 || j < 0)
+            return 0;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        int take = 0;
+        if(arr1[i] == arr2[j])
+            take = 1 + solve(i-1 , j-1 , arr1 , arr2);
+        max = Math.max(take , max);
+
+        solve(i-1 , j , arr1 , arr2);
+        solve(i , j-1 , arr1 , arr2);
+
+        return dp[i][j] = take;
+    }
     public int findLength(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
+
+        dp = new int[n][m];
+        for(int i=0; i<n; i++)
+            Arrays.fill(dp[i] , -1);
             
-        int max = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                
-                int x = i;
-                int y = j;
-                int count = 0;
-                
-                while(x<n && y<m && nums1[x] == nums2[y]){
-                    x++;
-                    y++;
-                    count++;
-                }
-                max = Math.max(max , count);
-            }
-        }
-        
+        solve(n-1 , m-1 , nums1 , nums2);
+
         return max;
     }
 }
