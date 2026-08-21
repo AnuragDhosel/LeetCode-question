0306644ef226d0e8b41075ea43f504473a546a70
Anurag dhosel <anuragdhosel04@gmail.com>
2026-08-21 19:53:59 +0530

    Time: 46 ms (6.19%), Space: 47.2 MB (62.33%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,21 +1,27 @@
 class Solution {
     int [][] dp;
-    public int longestPalindromicSubsequence(int i , int j , String s){
-        if(i > j) return 0;
-        if(i == j) return 1;
-
+    public int LongestCommonSubsequence(int i , int j , String s1 , String s2){
+        if(i < 0 || j < 0)
+            return 0;
+        
         if(dp[i][j] != -1)
             return dp[i][j];
 
-        int matched = 0;
-        if(s.charAt(i) == s.charAt(j))
-            matched = 2 + longestPalindromicSubsequence(i+1 , j-1 , s);
-        
-        int i_increase = longestPalindromicSubsequence(i+1 , j , s);
-        int j_decrease = longestPalindromicSubsequence(i , j-1 , s);
+        if(s1.charAt(i) == s2.charAt(j))
+            return dp[i][j] = 1 + LongestCommonSubsequence(i-1 , j-1 , s1 , s2);
+        else{
+            int i_decrease = LongestCommonSubsequence(i-1 , j , s1 , s2);
+            int j_decrease = LongestCommonSubsequence(i , j-1 , s1 , s2);
+  
+            return dp[i][j] = Math.max(i_decrease , j_decrease);
+        }
+    }
+    public int longestPalindromicSubsequence(String s){
+        StringBuilder sb = new StringBuilder(s);
+        String rev = sb.reverse().toString();
 
-        dp[i][j] = Math.max(matched , Math.max(i_increase , j_decrease));
-        return dp[i][j];
+        int n = s.length();
+        return LongestCommonSubsequence(n-1 , n-1 , s , rev);
     }
     public int minInsertions(String s) {
         int n = s.length();
@@ -24,8 +30,7 @@ class Solution {
         for(int i=0; i<n; i++)
             Arrays.fill(dp[i] , -1);
 
-        int LPS = longestPalindromicSubsequence(0 , n-1 , s);
-
+        int LPS = longestPalindromicSubsequence(s);
         return n - LPS;
     }
 }
