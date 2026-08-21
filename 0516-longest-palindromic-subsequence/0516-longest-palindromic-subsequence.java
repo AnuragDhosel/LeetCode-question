6fb26ea3b8ac05ddbead4f9a0bf9e5943c6ae1aa
Anurag dhosel <anuragdhosel04@gmail.com>
2026-08-21 19:23:16 +0530

    Time: 46 ms (52.96%), Space: 63.8 MB (47.71%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,29 +1,31 @@
 class Solution {
     int [][] dp;
-    public int solve(int i , int j , String s){
-        if(i > j) return 0;
-        if(i == j) return 1;
+    public int LongestCommonSubsequence(int i , int j , String s1 , String s2){
+        if(i < 0 || j < 0) 
+            return 0;
 
         if(dp[i][j] != -1)
             return dp[i][j];
 
-        int matched = 0;
-        if(s.charAt(i) == s.charAt(j))
-            matched = 2 + solve(i+1 , j-1 , s);
-        
-        int i_increase = solve(i+1 , j , s);
-        int j_decrease = solve(i , j-1 , s);
-
-        dp[i][j] = Math.max(matched , Math.max(i_increase , j_decrease));
-        return dp[i][j];
+        if(s1.charAt(i) == s2.charAt(j))
+            return dp[i][j] = 1 + LongestCommonSubsequence(i-1 , j-1 , s1 , s2);
+        else{
+            int i_decrease = LongestCommonSubsequence(i-1 , j , s1 , s2);
+            int j_decrease = LongestCommonSubsequence(i , j-1 , s1 , s2);
+  
+            return dp[i][j] = Math.max(i_decrease , j_decrease);
+        }
     }
     public int longestPalindromeSubseq(String s) {
         int n = s.length();
+
+        StringBuilder sb = new StringBuilder(s);
+        String rev = sb.reverse().toString(); 
 
         dp = new int[n][n];
         for(int i=0; i<n; i++)
             Arrays.fill(dp[i] , -1);
 
-        return solve(0 , n-1 , s);
+        return LongestCommonSubsequence(n-1 , n-1 , s , rev);
     }
 }
