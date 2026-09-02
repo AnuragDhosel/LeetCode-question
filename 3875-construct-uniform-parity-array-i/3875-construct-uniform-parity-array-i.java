e5dd56630858fca117d13206561418a07a764254
Anurag dhosel <anuragdhosel04@gmail.com>
2026-09-02 11:56:24 +0530

    Time: 1 ms (31.89%), Space: 45.2 MB (35.83%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        return true;
+        boolean isEven = false;
+        boolean isOdd = false;
+
+        for(int i=0; i<nums1.length; i++){
+            if(nums1[i] % 2 == 0){
+                isEven = true;
+            }
+            else{
+                isOdd = true;
+            }
+        }
+
+        return isEven || isOdd;
+
+
     }
 }
