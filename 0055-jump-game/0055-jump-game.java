49b6ed510ec6409f9dd582a67e6b8c29b8dfed43
Anurag dhosel <anuragdhosel04@gmail.com>
2026-09-08 23:18:38 +0530

    Time: 324 ms (6.56%), Space: 48.1 MB (23.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,30 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int maxJ = 0; // ith index se max kahan tak pahuch sakte hain
-        for(int i=0; i<nums.length; i++){
-            if(i > maxJ) // we can't reach i from starting point
-                return false;
+    int [] dp;
+    public boolean solve(int i , int [] arr){
+        if(i >= arr.length-1)
+            return true;
+        
+        if(dp[i] != -1)
+            return (dp[i] == 1) ? true : false;
 
-            maxJ = Math.max(maxJ , i + nums[i]);
+        boolean isReached = false;
+        for(int k=i+1; k<=i+arr[i]; k++){ // k is next idx
+            boolean curr = solve(k , arr);
+            if(curr){
+                isReached = true;
+                break;
+            }
         }
-        return true;
+
+        dp[i] = isReached ? 1 : 0;
+        return isReached;
+    }
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+
+        dp = new int[n+1];
+        Arrays.fill(dp , -1);
+
+        return solve(0 , nums);
     }
 }
