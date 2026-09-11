212bbf86da2f89b6d736f3b9bb6fd914c1d072f2
Anurag dhosel <anuragdhosel04@gmail.com>
2026-09-11 18:58:54 +0530

    Time: 12 ms (6.6%), Space: 43.5 MB (9.27%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,49 +1,34 @@
 class Solution {
     public String reorganizeString(String s) {
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
+        HashMap<Character , Integer> map = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(!map.containsKey(c)) map.put(c , 1);
+            else map.put(c , map.get(c) + 1);
         }
-        
-        int maxFreq = 0;
-        int maxLetter = 0;
-        
-        // Find the most frequent character
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > maxFreq) {
-                maxFreq = freq[i];
-                maxLetter = i;
-            }
-        }
-        
-        // If the max frequency is more than half the string (rounded up), it's impossible
-        if (maxFreq > (s.length() + 1) / 2) {
-            return "";
-        }
-        
-        char[] result = new char[s.length()];
-        int index = 0;
-        
-        // Place the most frequent character at even indices
-        while (freq[maxLetter] > 0) {
-            result[index] = (char) (maxLetter + 'a');
-            index += 2;
-            freq[maxLetter]--;
-        }
-        
-        // Place the remaining characters
-        for (int i = 0; i < 26; i++) {
-            while (freq[i] > 0) {
-                // If we reach the end of even indices, switch to odd indices
-                if (index >= s.length()) {
-                    index = 1;
+
+        StringBuilder sb = new StringBuilder();
+        char prevCh = '#';
+        while(map.size() > 0){
+            // find max freq character
+            int maxFreq = 0;
+            char currCh = '$'; // maxFreqChar
+            for(char key : map.keySet()){
+                if(maxFreq < map.get(key) && prevCh != key){
+                    maxFreq = map.get(key);
+                    currCh = key;
                 }
-                result[index] = (char) (i + 'a');
-                index += 2;
-                freq[i]--;
             }
+            prevCh = currCh;
+
+            sb.append(currCh);
+            if(currCh == '$') return "";
+            map.put(currCh , map.get(currCh) - 1);
+            
+            if(map.get(currCh) == 0) map.remove(currCh);
         }
-        
-        return new String(result);
+
+        return sb.toString();
     }
 }
