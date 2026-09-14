dc4e9d3d92728eef6ba6ce53d54c0ce7782e0454
Anurag dhosel <anuragdhosel04@gmail.com>
2026-09-14 16:41:56 +0530

    Time: 1 ms (19.11%), Space: 46.3 MB (83.19%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,24 +1,38 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int maxP = 0;
+        if(n == 1) return 0;
 
-        for(int i=0; i<n; i++){
-            int totalP = 0;
+        boolean [] floor = new boolean[n]; // buy
+        boolean [] ceil = new boolean[n]; // sell
 
-            int buy = prices[i];
-            for(int j=i+1; j<n; j++){
-                if(buy > prices[j]){
-                    buy = prices[j];
-                }
-                else if(buy < prices[j]){
-                    totalP += prices[j] - buy;
-                    buy = prices[j];
-                }
+        if(prices[0] < prices[1]) floor[0] = true;
+
+        for(int i=1; i<n-1; i++){
+            if(prices[i-1] < prices[i] && prices[i] >= prices[i+1]){ 
+                ceil[i] = true;
             }
-            maxP = Math.max(maxP , totalP);
+            else if(prices[i-1] >= prices[i] && prices[i] < prices[i+1]){ 
+                floor[i] = true;
+            }
         }
 
-        return maxP;
+        if(prices[n-2] < prices[n-1]) ceil[n-1] = true;
+
+        int buy = -1;
+        int sell = -1;
+        int profit = 0;
+        for(int i=0; i<n; i++){
+            if(floor[i] && !ceil[i]){
+                buy = prices[i];
+            }
+            else if(!floor[i] && ceil[i] && buy != -1){
+                sell = prices[i];
+                profit += sell - buy;
+                buy = -1;
+            }
+        }
+
+        return profit;
     }
 }
