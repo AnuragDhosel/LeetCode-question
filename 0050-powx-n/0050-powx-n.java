2338de531f8466bd6fa5165f183d9f5a24c8108a
Anurag dhosel <anuragdhosel04@gmail.com>
2026-09-23 00:50:00 +0530

    Time: 0 ms (100%), Space: 48 MB (45.76%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,13 @@
 class Solution {
-    public double myPow(double x, int n) {
-        return Math.pow(x , n);
+    public double myPow(double x, long n) {
+        if(n == 0)
+            return 1;
+        if(n < 0)
+            return myPow(1/x , -n);
+
+        if(n % 2 == 0)
+            return myPow(x * x , n/2);
+        else
+            return x * myPow(x , n-1);
     }
 }
